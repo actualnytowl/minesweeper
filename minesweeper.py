d7eed5877f4e01715e9822547c9daaa2aaf3954e
actualnytowl <53997065+actualnytowl@users.noreply.github.com>
2019-10-05 01:55:09 +0530

    Add files via upload

---
+++ minesweeper.py
@@ -0,0 +1,220 @@
+from tkinter import *
+from random import *
+from tkinter import messagebox
+boardsize = 10
+no_of_mines_to_be_placed = 20
+identified_bombs = 0
+
+mw = Tk()
+
+mi_bomb = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\bomb.png").subsample(2,2)
+mi_blank  = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\blank.png").subsample(8,8)
+mi_flag  = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\flagged.png").subsample(8,8)
+mi0 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\0.png").subsample(8,8)
+mi1 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\1.png").subsample(8,8)
+mi2 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\2.png").subsample(8,8)
+mi3 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\3.png").subsample(8,8)
+mi4 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\4.png").subsample(8,8)
+mi5 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\5.png").subsample(8,8)
+mi6 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\6.png").subsample(8,8)
+mi7 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\7.png").subsample(8,8)
+mi8 = PhotoImage(file = r"C:\Users\Sagar Annaji\Desktop\minesweeper-master\Images\8.png").subsample(8,8)
+
+button = {}
+board = {}
+revealed = {}
+flagged = {}
+bomb_index = set()
+
+for row in range(boardsize):
+    for col in range(boardsize):
+        board[(row,col)] = 0
+        revealed[(row, col)] = False
+        flagged[(row,col)] = False
+
+while(len(bomb_index) != no_of_mines_to_be_placed):
+     x = randrange(boardsize)
+     y = randrange(boardsize)
+     bomb_index.add((x,y))
+     board[x,y] = 1
+
+
+def exists(row, col):
+    if(row in range(boardsize) and col in range(boardsize)):
+        return (True)
+    else:
+        return (False)
+
+
+def no_of_mines(row,col):
+    count=0
+    if(exists(row-1,col) == True):
+        if(board[row-1,col]==1):
+            count+=1
+    if (exists(row + 1, col) == True):
+        if (board[row + 1, col] == 1):
+            count +=1
+    if (exists(row, col-1) == True):
+        if (board[row, col-1] == 1):
+            count +=1
+    if (exists(row, col+1) == True):
+        if (board[row, col+1] == 1):
+            count +=1
+    if (exists(row-1, col+1) == True):
+        if (board[row-1, col+1] == 1):
+            count +=1
+    if (exists(row-1, col-1) == True):
+        if (board[row-1, col-1] == 1):
+            count +=1
+    if (exists(row+1, col+1) == True):
+        if (board[row+1, col+1] == 1):
+            count +=1
+    if (exists(row+1, col-1) == True):
+        if (board[row+1, col-1] == 1):
+            count +=1
+    return (count)
+
+
+
+def zero(event,row,col):
+    x = row
+    y = col
+    if (exists(row - 1, col) == True and revealed[row-1,col]== False):
+        if (no_of_mines(row - 1, col) == 0):
+            button[row - 1, col].config(image=mi0)
+            revealed[x,y]=True
+            zero(event,x - 1, y)
+        else:
+            check(event,row - 1, col)
+    if (exists(row + 1, col) == True and revealed[row+1,col]== False):
+        if (no_of_mines(row + 1, col) == 0):
+            button[row + 1, col].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x + 1, y)
+        else:
+            check(event,row + 1, col)
+    if (exists(row, col - 1) == True and revealed[row,col-1]== False):
+        if (no_of_mines(row, col - 1) == 0):
+            button[row, col - 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x, y-1)
+        else:
+            check(event,row, col - 1)
+    if (exists(row, col + 1) == True and revealed[row,col+1]== False):
+        if (no_of_mines(row, col + 1) == 0):
+            button[row, col + 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x, y+1)
+        else:
+            check(event,row, col + 1)
+    if (exists(row - 1, col + 1) == True and revealed[row-1,col+1]== False):
+        if (no_of_mines(row - 1, col + 1) == 0):
+            button[row - 1, col + 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x - 1, y+1)
+        else:
+            check(event,row-1,col+1)
+    if (exists(row - 1, col - 1) == True and revealed[row-1,col-1]== False):
+        if (no_of_mines(row - 1, col - 1) == 0):
+            button[row - 1, col - 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x - 1, y-1)
+        else:
+            check(event,row - 1, col - 1)
+    if (exists(row + 1, col + 1) == True and revealed[row+1,col+1]== False):
+        if (no_of_mines(row + 1, col + 1) == 0):
+            button[row + 1, col + 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x + 1, y+1)
+        else:
+            check(event,row + 1, col + 1)
+    if (exists(row + 1, col - 1) == True and revealed[row+1,col-1]== False):
+        if (no_of_mines(row + 1, col - 1) == 0):
+            button[row + 1, col - 1].config(image=mi0)
+            revealed[x, y] = True
+            zero(event,x + 1, y-1)
+        else:
+            check(event,row + 1, col - 1)
+
+
+
+
+def check(event,x,y):
+    if (board[x, y] == 1):
+        for i in bomb_index:
+            button[i].config(image = mi_bomb)
+        if messagebox.showinfo("Result","CHI CHI YOU LOST") :
+            mw.destroy()
+            exit()
+    else:
+        mine_count = no_of_mines(x,y)
+        if(mine_count == 0):
+            row=x
+            col=y
+            #adj = [(row - 1, col),(row + 1, col),(row, col - 1),(row, col + 1),(row - 1, col + 1),(row - 1, col - 1),(row + 1, col + 1),(row + 1, col - 1)]
+            button[x, y].config(image=mi0)
+            revealed[x,y]= True
+            zero(event,x,y)
+        if(mine_count == 1):
+            button[x, y].config(image = mi1)
+            revealed[x, y] = True
+        if (mine_count == 2):
+            button[x, y].config(image = mi2)
+            revealed[x, y] = True
+        if (mine_count == 3):
+            button[x, y].config(image=mi3)
+            revealed[x, y] = True
+        if (mine_count == 4):
+            button[x, y].config(image=mi4)
+            revealed[x, y] = True
+        if (mine_count == 5):
+            button[x, y].config(image=mi5)
+            revealed[x, y] = True
+        if (mine_count == 6):
+            button[x, y].config(image=mi6)
+            revealed[x, y] = True
+        if (mine_count == 7):
+            button[x, y].config(image=mi7)
+            revealed[x, y] = True
+        if (mine_count == 8):
+            button[x, y].config(image=mi8)
+            revealed[x, y] = True
+
+
+
+def rightclick(event,row,col):
+    global identified_bombs
+    if(revealed[row,col]==False and flagged[(row,col)]==False):
+        button[row,col].config(image=mi_flag)
+        flagged[row,col] = True
+        if((row,col) in bomb_index):
+            identified_bombs+=1
+            #print(identified_bombs)
+
+    elif(flagged[(row,col)]==True):
+        button[row,col].config(image=mi_blank)
+        flagged[row,col]=False
+        if((row,col) in bomb_index):
+            identified_bombs-=1
+            #print(identified_bombs)
+
+    unopened_blocks=0
+    for p in range(boardsize):
+        for q in range(boardsize):
+            if(revealed[p,q] == False and (p,q) in bomb_index):
+                unopened_blocks+=1
+    if(unopened_blocks == no_of_mines_to_be_placed and identified_bombs == no_of_mines_to_be_placed):
+        if messagebox.showinfo("Result","YOU WON!!") :
+            mw.destroy()
+            exit()
+
+
+for row in range(boardsize):
+    for col in range (boardsize):
+        button[row,col] = Button(mw)#, command = lambda x=row, y=col : check(x,y))
+        button[row,col].bind("<Button-1>", lambda event, x=row, y=col : check(event,x,y))
+        button[row, col].bind("<Button-3>", lambda event, x=row,y=col : rightclick(event,x,y))
+        button[row,col].config(image = mi_blank)
+        button[row,col].grid(row = row,column = col)
+
+mainloop()
